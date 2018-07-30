d95733bf4daaf411479db35487f6660c244e34c1
sirromdev <sirromdev@gmail.com>
2018-07-30 13:19:06 +0100

    crude MST example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
-
-import '../node_modules/uikit/dist/css/uikit.min.css'
-import '../node_modules/uikit/dist/css/uikit-core.min.css'
-
-import '../node_modules/uikit/dist/js/uikit.min.js'
-import '../node_modules/uikit/dist/js/uikit-icons.min.js'
-import '../node_modules/uikit/dist/js/uikit-core.min.js'
-
-
-
+import React, { Component } from 'react'
+import SampleModelView from "./components/SampleModelView"
 
 class App extends Component {
-  render() {
+  render () {
+    console.log(this.props)
     return (
-      <div className="uk-container"> 
-        <h1 className="uk-heading-divider uk-text-center">Mobx truffle box</h1>
+      <div className='uk-container'> 
+        <h1 className='uk-heading-divider uk-text-center'>Mobx truffle box</h1>
+
+        <SampleModelView sampleModel={this.props.sampleModel}/>
       </div>
-    );
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './assets/index.css'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ********- CSS FRAMEWORK -********
+import '../node_modules/uikit/dist/css/uikit.min.css'
+import '../node_modules/uikit/dist/css/uikit-core.min.css'
+import '../node_modules/uikit/dist/js/uikit.min.js'
+import '../node_modules/uikit/dist/js/uikit-icons.min.js'
+import '../node_modules/uikit/dist/js/uikit-core.min.js'
+// ********- ************* -********
+
+import { SampleModel } from "./models/sampleModel"
+
+const sampleModel = SampleModel.create({
+  items: [
+    {
+      name: "Machine Gun Preacher",
+      price: 7.35,
+      author: "T.E Lawrence"
+    },
+    {
+      name: "LEGO Mindstorm EV3",
+      price: 349.95,
+      author: "Julie Shields"
+    }
+  ]
+})
+ReactDOM.render(<App sampleModel={sampleModel}/>, document.getElementById('root'))
+
+setInterval(() => {
+  sampleModel.items[0].changePrice(sampleModel.items[0].price + 1)
+}, 1000)
