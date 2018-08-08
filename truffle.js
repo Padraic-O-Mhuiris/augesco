0357e289a35e21f920719a6e2908e5380594180d
sirromdev <sirromdev@gmail.com>
2018-08-08 19:23:06 +0100

    imported private data from removed truffle-config.js

--- truffle.js
+++ truffle.js
@@ -1,6 +1,5 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const mnemonic = "";
-const INFURA_Access_Token = ""
+const config = require('./truffle-config')
 
 
 module.exports = {
@@ -13,14 +12,14 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + INFURA_Access_Token)
+        return new HDWalletProvider(config.MNEMONIC, "https://ropsten.infura.io/" + config.INFURA_KEY)
       },
       network_id: 3,
       gas: 5786536
     },
     rinkeby: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/" + INFURA_Access_Token)
+        return new HDWalletProvider(config.MNEMONIC, "https://rinkeby.infura.io/" + config.INFURA_KEY)
       },
       network_id: 4,
       gas: 4712356
