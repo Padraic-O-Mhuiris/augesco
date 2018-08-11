1443ba55da0437e3ff91dbeaebd2e77466462be7
sirromdev <sirromdev@gmail.com>
2018-08-11 21:05:55 +0100

    imported Uikit js module and notifying on transaction states

--- src/components/eventComponents/eventGate.js
+++ src/components/eventComponents/eventGate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { inject, observer } from "mobx-react"
 import { txStatus } from '../../constants';
+import UIkit from 'uikit'
 
 @inject("web3Store")
 @inject("contractStore")
@@ -15,22 +16,48 @@ import { txStatus } from '../../constants';
 
     contractStore.txEmitter.on(txStatus.NEW, (hash) => {
       console.log("Transaction Hash", hash)
+      UIkit.notification({
+        message: 'New Transaction',
+        pos: 'bottom-right',
+        status: 'secondary'
+      })
+
     })
 
     contractStore.txEmitter.on(txStatus.PENDING, (data) => {
       console.log("Transaction Pending", data)
+      UIkit.notification({
+        message: 'Transaction Pending',
+        pos: 'bottom-right',
+        status: 'secondary'
+      })
     })
 
     contractStore.txEmitter.on(txStatus.MINED, (data) => {
       console.log("Transaction Mined", data)
+      UIkit.notification({
+        message: 'Transaction Mined',
+        pos: 'bottom-right',
+        status: 'primary'
+      })
     })
 
     contractStore.txEmitter.on(txStatus.FAILED, (data) => {
       console.log("Transaction Failed", data)
+      UIkit.notification({
+        message: 'Transaction Failed',
+        pos: 'bottom-right',
+        status: 'danger'
+      })
     })
 
     contractStore.txEmitter.on(txStatus.SUCCESS, (data) => {
       console.log("Transaction Success", data)
+      UIkit.notification({
+        message: 'Transaction Success',
+        pos: 'bottom-right',
+        status: 'success'
+      })
     })
     
     contractStore.listen("Counter", "Increment", {}, ((err, event) => {
