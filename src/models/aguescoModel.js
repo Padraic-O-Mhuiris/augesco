2d56fb193dbf108e0f1f37451a3f721bf901b7fd
sirromdev <sirromdev@gmail.com>
2018-08-30 21:14:30 +0100

    created instances file and merged contractModel into augescoModel

--- src/models/aguescoModel.js
+++ src/models/aguescoModel.js
@@ -2,6 +2,9 @@ import { types, flow } from "mobx-state-tree";
 import { web3Context } from "../constants";
 import { BigNumber } from 'bignumber.js';
 import getWeb3Network from "../utils/getWeb3Network"
+import { contractInstance } from "./instanceContract"
+import { transactionInstance } from "./instanceTx"
+import { txStatus } from "../constants"
 
 const web3Contexts = [
   web3Context.WEB3_LOAD_ERR,
@@ -15,9 +18,6 @@ const web3Contexts = [
 let ptx = null;
 let nbh = null;
 
-const contractInstance = {};
-const txInstance = {};
-
 export const AugescoStore = types
   .model({
     account: types.optional(types.string, ""),
@@ -28,11 +28,29 @@ export const AugescoStore = types
     web3_ws: types.optional(types.frozen()),
     witness: types.optional(types.frozen(), {}),
     contracts: types.map(contractInstance),
-    transactions: types.map(txInstance),
+    transactions: types.map(transactionInstance),
     loaded: false,
-    ethData: false
+    info: false
   })
   .actions(self => ({
+    add(_id, _abi, _txHash, _address, _contract, _methods, _eventContract, _events) {
+      self.contracts.set(_id, {
+        name: _id,
+        abi: _abi,
+        txHash: _txHash,
+        address: _address,
+        contract: _contract,
+        methods: _methods,
+        eventContract: _eventContract,
+        events: _events
+      })
+    },
+    toggleLoaded() {
+      self.loaded = !self.loaded
+    },
+    toggleInfo() {
+      self.info = !self.info
+    },
     setWeb3(web3) {
       self.web3 = web3;
     },
@@ -43,7 +61,7 @@ export const AugescoStore = types
       self.account = account;
       self.updateStatus(web3Context.WEB3_LOADED);
     },
-    setEmitter(emitter) {
+    setWitness(emitter) {
       self.witness = emitter;
     },
     updateStatus(status) {
@@ -54,6 +72,71 @@ export const AugescoStore = types
     },
     updateNetwork(network) {
       self.network = network;
+    },
+    use(_id) {
+      if (self.loaded && self.contracts.has(_id)) {
+        return self.contracts.get(_id)
+      } else {
+        return {}
+      }
+    },
+    getMethod(_id, _method) {
+      if (self.loaded && self.contracts.has(_id)) {
+        return self.use(_id).getMethod(_method)
+      } else {
+        return {}
+      }
+    },
+    createTx(_id) {
+      self.transactions.set(_id, {
+        hash: _id,
+        receipt: {},
+        status: txStatus.PENDING
+      })
+    },
+    useTx(_id) {
+      if (self.loaded && self.transactions.has(_id)) {
+        return self.transactions.get(_id)
+      } else {
+        return {}
+      }
+    },
+    startTx(_id) {
+      self.useTx(_id).txStart()
+    },
+    call: flow(function* call(_id, _method, _args) {
+      if (self.loaded && self.contracts.has(_id)) {
+        try {
+          return yield self.getMethod(_id, _method)["func"](..._args).call()
+        } catch (error) {
+          console.error(error)
+        }
+      } else {
+        return undefined
+      }
+    }),
+    exec: flow(function* exec(_id, _method, _args, _params) {
+      if (self.loaded && self.contracts.has(_id)) {
+        try {
+          yield self.getMethod(_id, _method)["func"](..._args)
+            .send(_params)
+            .on('transactionHash', function (hash) {
+              self.createTx(hash)
+              self.startTx(hash)
+            })
+        } catch (error) {
+          console.error(error)
+        }
+      } else {
+        console.error("Not ready")
+      }
+    }),
+    listen(_id, _event, _options, _cb) {
+      if (self.loaded && self.contracts.has(_id)) {
+        self.use(_id).events[_event](..._options, _cb)
+      } else {
+        console.error("Not ready")
+      }
     },
     determineNetwork: flow(function* determineNetwork() {
       try {
@@ -114,5 +197,14 @@ export const AugescoStore = types
     },
     get netName() {
       return getWeb3Network(self.network);
+    },
+    get keys() {
+      return Array.from(self.contracts.keys())
+    },
+    get values() {
+      return Array.from(self.contracts.values())
+    },
+    get json() {
+      return self.contracts.toJSON()
     }
   }));
