7e3ce4f0e7e9f49893593bbb37b6841e0e36bf83
sirromdev <sirromdev@gmail.com>
2018-08-31 15:19:30 +0100

    removed contractStore and web3Store models

--- src/index.js
+++ src/index.js
@@ -2,24 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { web3Context } from "./constants"
-import { Web3Store } from "./models/web3Model"
-import { ContractStore } from "./models/contractModel"
 import { AugescoStore } from "./models/aguescoModel"
 import { Provider } from "mobx-react"
 import { onPatch } from "mobx-state-tree"
 
-const web3Store = Web3Store.create({
-  name: "",
-  web3: {},
-  status: web3Context.WEB3_LOADING,
-  balance: "0",
-  network: 0
-})
-
-const contractStore = ContractStore.create({
-  contracts: {},
-  loaded: false,
-})
 
 const augesco = AugescoStore.create({
   account: "",
@@ -34,8 +20,6 @@ onPatch(augesco, patch => {
 })
 const app = (
   <Provider
-    web3Store={web3Store}
-    contractStore={contractStore}
     augesco={augesco}
   >
     <App />
