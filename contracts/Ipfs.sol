1b79ebe3842395982a02ec0eaf8a9ede676cd3b5
sirromdev <sirromdev@gmail.com>
2018-09-04 13:11:15 +0100

    added upload event

--- contracts/Ipfs.sol
+++ contracts/Ipfs.sol
@@ -2,9 +2,14 @@ pragma solidity ^0.4.24;
 
 contract Ipfs {
   string ipfsHash;
- 
+
+  event NewHash(
+    string indexed newHash
+  );
+
   function setHash(string x) public {
     ipfsHash = x;
+    emit NewHash(x);
   }
 
   function getHash() public view returns (string x) {
