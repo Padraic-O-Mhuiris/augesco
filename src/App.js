cfe683431ddadfdb8b9a6a86f40b3144f30e5824
sirromdev <sirromdev@gmail.com>
2018-09-11 15:19:52 +0100

    added router object to props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import Web3Gate from "./components/web3Gate";
 import "./assets/less/index";
-
+import { withRouter } from 'react-router';
+import { inject, observer } from 'mobx-react';
 import AppContent from "./components/appComponents/appContent";
 
 const contractCounter = require("../build/contracts/Counter.json");
 const contractIpfs = require("../build/contracts/Ipfs.json");
 
+@inject("router")
 class App extends Component {
   render() {
+    const { location, push, goBack } = this.props.router;
     return (
       <Web3Gate
         contracts={[contractCounter, contractIpfs]}
@@ -23,10 +26,11 @@ class App extends Component {
           protocol: "https"
         }}
       >
+        <button onClick={() => push('/test')}>Change url</button>
         <AppContent />
       </Web3Gate>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
