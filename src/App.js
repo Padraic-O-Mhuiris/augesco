4b419be454cc2e8159684e9d9062b8393d732f2d
sirromdev <sirromdev@gmail.com>
2018-09-14 18:59:31 +0100

    added simple navbar outside of routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import Web3Gate from './components/augesco/web3Components/web3Gate';
 import './assets/less/index';
 import { withRouter, Route } from 'react-router';
+import { Layout, Menu } from 'antd';
 
 import Landing from './components/appComponents/landing';
-import Docs from './components/appComponents/docs'
-import Reporter from './components/appComponents/reporter'
+import Docs from './components/appComponents/docs';
+import Reporter from './components/appComponents/reporter';
+
+const { Header } = Layout;
 
 const contractCounter = require('../build/contracts/Counter.json');
 const contractIpfs = require('../build/contracts/Ipfs.json');
@@ -18,11 +21,11 @@ const routes = [
   },
   {
     path: '/docs',
-    main: () => <Docs/>
+    main: () => <Docs />
   },
   {
     path: '/reporter',
-    main: () => <Reporter/>
+    main: () => <Reporter />
   }
 ];
 
@@ -42,14 +45,31 @@ class App extends Component {
           protocol: 'https'
         }}
       >
-        {routes.map((route, index) => (
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            component={route.main}
-          />
-        ))}
+        <Layout>
+          <Header className="header">
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+          </Header>
+          <Layout>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                component={route.main}
+              />
+            ))}
+          </Layout>
+        </Layout>
       </Web3Gate>
     );
   }
