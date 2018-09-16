8070b1bd7a9976fefc9b32f881bd181a68017741
sirromdev <sirromdev@gmail.com>
2018-09-16 12:24:38 +0100

    abstracted header to render on route context

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Web3Gate from './components/augesco/web3Components/web3Gate';
 import './assets/less/index';
 import { withRouter, Route } from 'react-router';
-import { Layout, Menu } from 'antd';
+import { Layout } from 'antd';
 
 import Landing from './components/appComponents/landing';
 import Docs from './components/appComponents/docs';
 import Reporter from './components/appComponents/reporter';
 
 import SideNav from './components/appComponents/sideNav';
+import NavBar from './components/appComponents/navBar';
 const { Header } = Layout;
 
 const contractCounter = require('../build/contracts/Counter.json');
@@ -18,15 +19,18 @@ const routes = [
   {
     path: '/',
     exact: true,
+    header: () => <NavBar />,
     main: () => <Landing />
   },
   {
     path: '/docs',
+    header: () => <NavBar />,
     main: () => <Docs />,
     sider: () => <SideNav />
   },
   {
     path: '/reporter',
+    header: () => <NavBar />,
     main: () => <Reporter />
   }
 ];
@@ -48,19 +52,14 @@ class App extends Component {
         }}
       >
         <Layout>
-          <Header className="header">
-            <div className="logo" />
-            <Menu
-              theme="dark"
-              mode="horizontal"
-              defaultSelectedKeys={['2']}
-              style={{ lineHeight: '64px' }}
-            >
-              <Menu.Item key="1">nav 1</Menu.Item>
-              <Menu.Item key="2">nav 2</Menu.Item>
-              <Menu.Item key="3">nav 3</Menu.Item>
-            </Menu>
-          </Header>
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.header}
+            />
+          ))}
           <Layout>
             {routes.map((route, index) => (
               <Route
